a58077068b5ecbe843f9436e80ef240bfd73b865
martijnkunstman <martijn@kunstman.nl>
2022-12-05 09:15:35 +0100

    init

---
+++ main.js
@@ -0,0 +1,53 @@
+//
+// PHP
+// C#, C++, C
+// Python
+// Rust
+// Java
+//
+// JavaScript
+//
+
+
+//variabelen
+
+var laatje1; //variabele
+var laatje2; //let, const
+var laatje3;
+
+laatje1 = "Adil"; //String
+laatje2 = 17; //nummer
+laatje3 = "kerstman";
+
+//laatje3 = true; //boolean
+//laatje3 = ["a", "b", 1, 2]; //array
+//laatje3 = {a:"a",b:1};//object
+
+console.log(laatje1);
+console.log(laatje2);
+console.log(laatje3);
+
+// if statement
+if (laatje3=="sinterklaas")
+{
+    console.log("laatje 3 is sinterklaas");
+}
+else
+{
+    console.log("laatje 3 is NIET sinterklaas");
+}
+
+// output
+document.getElementById("eersteDiv").innerHTML = laatje3;
+
+// input
+// onclick
+
+//for loops
+for (var i = 0; i<10;i++)
+{
+    console.log(i);
+}
+
+// function
+
